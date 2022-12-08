7600de21f035db976f20ff213bf6a96985dc1c43
Lucasdescatoire <72078978+Lucasdescatoire@users.noreply.github.com>
2022-12-08 11:06:31 +0100

    fix Bug hunt dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -146,7 +146,9 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#status-bills-container${this.index} #open-bill${bill.id}`).click((event) => {
+        this.handleEditTicket(event, bill, bills)
+      })
     })
 
     return bills
